09c98a4d39d405e497b0c5fa82b5b950839cbad0
Haroon Ahmad <haroon334@gmail.com>
2022-01-10 12:28:43 +0100

    Lets go

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,62 +1,71 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { useAuth } from '../../context/auth/reducer'
+import { useAuth } from "../../context/auth/reducer";
 
 export default function Register() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const { loading, login, currentUser } = useAuth();
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const { loading, login, currentUser } = useAuth();
+  const navigate = useNavigate();
 
-    async function handleSubmit(e) {
-        e.preventDefault();
-        try {
-            await login(email, password);
-            navigate('/home');
-        } catch {
-            setError('Something went wrong')
-        }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      await login(email, password);
+      navigate("/home");
+    } catch {
+      setError("Something went wrong");
     }
+  }
 
-    return (
-        <div className="container p-5">
-            <h2 className='text-center'>Log In</h2>
-            {error && <p className='text-center danger'>{error}</p>}
-            <div className="row">
-                <div className="col-md-6 mx-auto">
-                    <form onSubmit={handleSubmit}>
-                        <div className="form-group">
-                            <label htmlFor="inputEmail">Email</label>
-                            <input
-                                type="email"
-                                className="form-control"
-                                id="inputEmail" placeholder="Email"
-                                value={email}
-                                onChange={e => setEmail(e.target.value)}
-                                autoFocus
-                                required
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="inputPassword">Password</label>
-                            <input
-                                type="password"
-                                className="form-control"
-                                id="inputPassword" placeholder="Password"
-                                value={password}
-                                onChange={e => setPassword(e.target.value)}
-                                autoFocus
-                                required
-                            />
-                        </div>
-                        <button type="submit" className="btn btn-primary">Log in</button>
-                    </form>
-                </div>
-                <p className='text-center'>Don't have an account? Register <a href="/register">here</a></p>
-                <p className='text-center'>Forgot your password? Reset your password <a href="/recoverpassword">here</a></p>
+  return (
+    <div className="container p-5">
+      <h2 className="text-center">Log In</h2>
+      {error && <p className="text-center danger">{error}</p>}
+      <div className="row">
+        <div className="col-md-6 mx-auto">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="inputEmail">Email</label>
+              <input
+                type="email"
+                className="form-control"
+                id="inputEmail"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                autoFocus
+                required
+              />
             </div>
+            <div className="form-group">
+              <label htmlFor="inputPassword">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                id="inputPassword"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                autoFocus
+                required
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Log in
+            </button>
+          </form>
         </div>
-    )
+        <p className="text-center">
+          Don't have an account? Register <a href="/register">here</a>
+        </p>
+        <p className="text-center">
+          Forgot your password? Reset your password{" "}
+          <a href="/recoverpassword">here</a>
+        </p>
+      </div>
+    </div>
+  );
 }
