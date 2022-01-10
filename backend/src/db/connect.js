5ec5c916d7f786eec1424c488af2c1abc1b534f6
sergi92 <45389112+sergi92@users.noreply.github.com>
2022-01-10 12:51:25 +0100

    fixed mongodb connection

--- backend/src/db/connect.js
+++ backend/src/db/connect.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const config = require('../config/config');
 
 function connect() {
+
   // return mongoose.connect("mongodb://localhost:27017/node-mvc-react", {
   return mongoose.connect(`${config.db.url}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+
   });
 }
 
