b4571da19ff051f8ca5c7ca2409c69b81a881515
sergi92 <45389112+sergi92@users.noreply.github.com>
2022-01-11 12:22:49 +0100

    fixed location issue

--- client/src/components/UI/molecules/NavList/NavList.js
+++ client/src/components/UI/molecules/NavList/NavList.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./NavList.scss";
 
 export default function NavList() {
+  let location = useLocation();
   return (
     <>
       <div id="checkout-steps">
